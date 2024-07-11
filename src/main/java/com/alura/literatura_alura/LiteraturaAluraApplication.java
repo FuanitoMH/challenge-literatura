64c9ccec225d98c45b5e9bb08f0b517903c44673
FUANITO2029 <juanito2029@live.com.mx>
2024-07-11 14:04:08 -0600

    first commit

---
+++ src/main/java/com/alura/literatura_alura/LiteraturaAluraApplication.java
@@ -0,0 +1,13 @@
+package com.alura.literatura_alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturaAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturaAluraApplication.class, args);
+	}
+
+}
